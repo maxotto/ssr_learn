05b0d436b7421fac11c00d7f4a1de9c84137f2ae
Andrey Mikhailichenko <andrey@mikhailichenko.name>
2019-11-03 21:03:14 +0300

    router-view

--- src/App.vue
+++ src/App.vue
@@ -17,22 +17,16 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
-  })
+  name: "App"
 });
 </script>
